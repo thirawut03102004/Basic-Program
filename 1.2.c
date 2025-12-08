2b9cfe6e6eb153ac2eed5519f49d9269e49bab49
unknown <talonpopy@gmail.com>
2025-12-08 12:46:51 +0700

    1.2

---
+++ 1.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    printf("Enter your Score : ");
+    scanf("%d", &score);
+    if (score>=80){
+    printf("Grade A\n");
+    }
+    else if (score>=70){
+    printf("Grade B\n");
+    }
+    else if (score>=60){
+    printf("Grade C\n");
+    }
+    else if (score>=50){
+    printf("Grade D\n");
+    }
+    else{
+    printf("Grade F\n");
+    }
+    return 0;
+}
