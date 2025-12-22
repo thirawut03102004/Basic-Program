8c7169e0dcbe2c04f2b958c9ea5aa72076876d0f
unknown <talonpopy@gmail.com>
2025-12-22 14:52:17 +0700

    1.5

--- 1.5.c
+++ 1.5.c
@@ -5,24 +5,23 @@ int main() {
     int evenSum = 0;
     int oddSum = 0;
 
-    printf("กรอกตัวเลขจำนวนเต็ม (0 เพื่อจบ):\n");
+    printf("Enter (0 for End): \n");
+    
+    if (scanf("%d", &number) != 1) {
+        return 1;
+    }
 
-    do {
-        printf("กรอกเลข: ");
+    while (number != 0) {
+        if (number % 2 == 0) {
+            evenSum += number; 
+        } else {
+            oddSum += number;
+        }
         if (scanf("%d", &number) != 1) {
-            break; 
+            break;
         }
+    }
 
-        if (number != 0) {
-            if (number % 2 == 0) {
-                evenSum += number; 
-            } 
-            else {
-                oddSum += number;  
-            }
-        }
-
-    } while (number != 0);
     printf("Even Sum: %d\n", evenSum);
     printf("Odd Sum: %d\n", oddSum);
 
