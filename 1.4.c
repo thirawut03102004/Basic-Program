ef239c982f077a141c0dd14bee28ca4599096011
unknown <talonpopy@gmail.com>
2025-12-22 13:13:07 +0700

    1.4

--- 1.4.c
+++ 1.4.c
@@ -2,31 +2,17 @@
 
 int main()
 {
-    int num1, num2,operationCode,result;
+    int N, sum =0,i;
+    printf("Enter Number : ");
+    if (scanf("%d", &N) !=1)
+    {
+        return 1;
+    }
+    for (i = 0; i <= N; i++)
+    {
+        sum += i;
+    }
+    printf("%d\n",sum);
     
-    printf("Enter your number 1,2 and operation : ");
-    scanf("%d", &num1);
-    printf("Enter your number 1,2 and operation : ");
-    scanf("%d", &num2);
-    printf("Enter your number 1,2 and operation : ");
-    if (scanf("%d %d %d", &num1,&num2,&operationCode) >=0){
-    switch (operationCode) {
-    case 1:
-        result= num1 + num2;
-    case 2:
-        result= num1 - num2;
-        
-    case 3:
-        result= num1 * num2;
-    case 4:
-        result= num1 / num2;
-    default:
-    break;
-    }
-}
-    else{
-    printf("Invalid Operation");
-    }
-    printf("%d",result);
     return 0;
 }
