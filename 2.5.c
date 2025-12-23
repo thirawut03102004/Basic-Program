e1b994baab3c2908fa563a1fe7ccb355c1a9ab8d
unknown <talonpopy@gmail.com>
2025-12-23 18:46:20 +0700

    2.5

--- 2.5.c
+++ 2.5.c
@@ -3,15 +3,10 @@
 int main() {
     int score;
     int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
-
-    // รับค่าคะแนนตัวแรก
     if (scanf("%d", &score) != 1) {
         return 1;
     }
-
-    // วนลูปรับค่าไปเรื่อยๆ จนกว่าจะรับค่า -1
     while (score != -1) {
-        // เงื่อนไขการให้เกรด
         if (score >= 80) {
             countA++;
         } else if (score >= 70) {
@@ -23,8 +18,6 @@ int main() {
         } else {
             countF++;
         }
-
-        // รับค่าคะแนนตัวถัดไปภายในลูป
         if (scanf("%d", &score) != 1) {
             break;
         }
