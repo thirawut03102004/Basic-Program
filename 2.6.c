2dd9b3453a9ca1d4073d8f64b39fcbfab3a03d00
unknown <talonpopy@gmail.com>
2025-12-23 19:12:20 +0700

    2.6

--- 2.6.c
+++ 2.6.c
@@ -4,26 +4,32 @@ int main() {
     int N_MONTHS, month, successCount = 0;
     float dailyDeposit, monthlyTotal;
     if (scanf("%d", &N_MONTHS) != 1) {
+        printf("Error\n");
         return 1;
     }
+
     for (month = 1; month <= N_MONTHS; month++) {
-        monthlyTotal = 0.0; 
+        monthlyTotal = 0.0;
         if (scanf("%f", &dailyDeposit) != 1) {
-            break;
+            printf("Error\n");
+            return 1;
         }
         while (dailyDeposit != 0.00) {
             monthlyTotal += dailyDeposit;
+
             if (scanf("%f", &dailyDeposit) != 1) {
-                break;
+                printf("Error\n");
+                return 1;
             }
         }
+
         if (monthlyTotal >= 500.00) {
-            successCount++; 
+            successCount++;
         }
+
         printf("Month %d Total: %.2f\n", month, monthlyTotal);
     }
 
     printf("Success Count: %d\n", successCount);
-
     return 0;
 }
