c5821a6c0d67aa95ebb3168e34e925fccee18b7b
unknown <talonpopy@gmail.com>
2025-12-23 19:07:29 +0700

    2.5

--- 2.5.c
+++ 2.5.c
@@ -3,26 +3,22 @@
 int main() {
     int score;
     int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
-    if (scanf("%d", &score) != 1) {
-        return 1;
-    }
-    while (score != -1) {
-        if (score >= 80) {
-            countA += 1;
-        } else if (score >= 70) {
-            countB += 1;
-        } else if (score >= 60) {
-            countC += 1;
-        } else if (score >= 50) {
-            countD += 1;
-        } else {
-            countF += 1;
+    printf("Enter scores (-1 to stop): \n");
+    while (1) {
+        int result = scanf("%d", &score);
+        if (result == 0) {
+            printf("Error: Invalid input! Please enter only numbers.\n");
+            return 1;
         }
-        if (scanf("%d", &score) != 1) {
+        if (score == -1) {
             break;
         }
+        if (score >= 80) {countA += 1;} 
+        else if (score >= 70) {countB += 1;} 
+        else if (score >= 60) {countC += 1;} 
+        else if (score >= 50) {countD += 1;} 
+        else if (score >= 0) {countF += 1;}
     }
-
     printf("Grade A Count: %d\n", countA);
     printf("Grade B Count: %d\n", countB);
     printf("Grade C Count: %d\n", countC);
