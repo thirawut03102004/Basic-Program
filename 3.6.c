8a28d8626e6c95ab5d6a3c671c6e89d97083532f
Thirawut <talonpopy@gmail.com>
2025-12-23 21:59:18 +0700

    Update 3.6.c

--- 3.6.c
+++ 3.6.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 
-struct Transaction {
+struct Transaction
+{
     char description[50];
     float amount;
 };
 
-void calculateTotals(struct Transaction t[], int n, float *income, float *expense) {
-    for (int i = 0; i < n; i++) {
-        if (t[i].amount >= 0.0) {
-            *income += t[i].amount;
-        } else {
-            *expense += -t[i].amount;
-        }
-    }
-}
-
-int main() {
+int main()
+{
     int N;
     float totalIncome = 0.0, totalExpense = 0.0, netBalance = 0.0;
 
-    if (scanf("%d", &N) != 1) return 1;
+    if (scanf("%d", &N) != 1)
+        return 1;
 
     struct Transaction transactions[N];
 
-    for (int i = 0; i < N; i++) {
-        scanf("%f %s", &transactions[i].amount, transactions[i].description);
+    for (int i = 0; i < N; i++)
+    {
+        if (scanf("%f %s", &transactions[i].amount, transactions[i].description) == 2)
+        {
+            if (transactions[i].amount >= 0)
+            {
+                totalIncome += transactions[i].amount;
+            }
+            else
+            {
+                totalExpense += -transactions[i].amount;
+            }
+        }
     }
-
-    calculateTotals(transactions, N, &totalIncome, &totalExpense);
 
     netBalance = totalIncome - totalExpense;
 
