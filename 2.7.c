fa24774df8a06cd141981165fa7df84898689d2a
Thirawut <talonpopy@gmail.com>
2025-12-23 22:36:46 +0700

    Update 2.7.c

--- 2.7.c
+++ 2.7.c
@@ -1,30 +1,42 @@
 #include <stdio.h>
 
-int main() {
+void checkFizzBuzz(int val, int *fb, int *f, int *b, int *ot)
+{
+    if (val % 15 == 0)
+    {
+        *fb = *fb + 1;
+    }
+    else if (val % 3 == 0)
+    {
+        *f = *f + 1;
+    }
+    else if (val % 5 == 0)
+    {
+        *b = *b + 1;
+    }
+    else
+    {
+        *ot = *ot + 1;
+    }
+}
+
+int main()
+{
     int N, i;
     int countFizzBuzz = 0;
     int countFizz = 0;
     int countBuzz = 0;
     int countOther = 0;
 
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1)
+    {
         printf("Error\n");
         return 1;
     }
 
-    for (i = 1; i <= N; i++) {
-        if (i % 15 == 0) {
-            countFizzBuzz++;
-        }
-        else if (i % 3 == 0) {
-            countFizz++;
-        }
-        else if (i % 5 == 0) {
-            countBuzz++;
-        }
-        else {
-            countOther++;
-        }
+    for (i = 1; i <= N; i++)
+    {
+        checkFizzBuzz(i, &countFizzBuzz, &countFizz, &countBuzz, &countOther);
     }
 
     printf("Count FizzBuzz (by 15): %d\n", countFizzBuzz);
