129a6e8e612c5b8642993a47954bb855582bb70d
Thirawut <talonpopy@gmail.com>
2025-12-23 22:34:21 +0700

    Update 3.12.c

--- 3.12.c
+++ 3.12.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
 
+float cal(int lvl, int armor)
+{
+    float baseAtk = lvl * 10.0;
+    float reduction;
+
+    if (armor < 50)
+    {
+        reduction = 0.20;
+    }
+    else
+    {
+        reduction = 0.40;
+    }
+
+    return baseAtk * (1.0 - reduction);
+}
+
 int main()
 {
     int N_CALCULATIONS, i;
     int attkLvl, oppLvl, oppArmor;
-    float baseAttack, damageReduction, netDamage, damageDeficit;
+    float netDamage, damageDeficit;
     const float WORTH_THRESHOLD = 150.0;
 
     if (scanf("%d", &N_CALCULATIONS) != 1)
@@ -17,18 +34,7 @@ int main()
         if (scanf("%d %d %d", &attkLvl, &oppLvl, &oppArmor) == 3)
         {
 
-            baseAttack = attkLvl * 10.0;
-
-            if (oppArmor < 50)
-            {
-                damageReduction = 0.20;
-            }
-            else
-            {
-                damageReduction = 0.40;
-            }
-
-            netDamage = baseAttack * (1.0 - damageReduction);
+            netDamage = cal(attkLvl, oppArmor);
 
             if (netDamage >= WORTH_THRESHOLD)
             {
