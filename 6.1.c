1b014adfe2f2d001777f6c4bd660149b3599205c
Thirawut <talonpopy@gmail.com>
2025-12-24 20:17:50 +0700

    Create 6.1.c

---
+++ 6.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void increase_value(int *number_ptr)
+{
+    *number_ptr = *number_ptr + 10;
+}
+
+int main()
+{
+    int data;
+
+    printf("Enter initial integer value: ");
+    scanf("%d", &data);
+
+    printf("\n--- POINTER MODIFICATION REPORT ---\n");
+    printf("1. Value BEFORE function call: %d\n", data);
+
+    increase_value(&data);
+
+    printf("2. Value AFTER function call: %d\n", data);
+
+    return 0;
+}
