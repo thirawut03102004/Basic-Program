b467810513d076229063125168b0facbd59c8ed6
Thirawut <talonpopy@gmail.com>
2025-12-24 19:35:46 +0700

    Update 5.5.c

--- 5.5.c
+++ 5.5.c
@@ -1,48 +1,40 @@
 #include <stdio.h>
 
-#define NUM_STUDENTS 4
-
 struct Student
 {
     int id;
 };
 
-int check(struct Student students[], int size, int search_id)
-{
-    for (int i = 0; i < size; i++)
-    {
-        if (students[i].id == search_id)
-        {
-            return 1;
-        }
-    }
-    return 0;
-}
-
 int main()
 {
-    struct Student id_list[NUM_STUDENTS];
+    struct Student id_list[4];
     int search_id;
-    int found_status;
+    int found_status = 0;
     int i;
 
-    for (i = 0; i < NUM_STUDENTS; i++)
+    for (i = 0; i < 4; i++)
     {
         scanf("%d", &id_list[i].id);
     }
 
     scanf("%d", &search_id);
 
-    found_status = check(id_list, NUM_STUDENTS, search_id);
+    for (i = 0; i < 4; i++)
+    {
+        if (id_list[i].id == search_id)
+        {
+            found_status = 1;
+            break;
+        }
+    }
 
     printf("\n--- ID SEARCH REPORT ---\n");
     printf("Recorded IDs: ");
-    for (i = 0; i < NUM_STUDENTS; i++)
+    for (i = 0; i < 4; i++)
     {
         printf("%d ", id_list[i].id);
     }
-    printf("\n");
-    printf("Search ID: %d\n", search_id);
+    printf("\nSearch ID: %d\n", search_id);
 
     if (found_status == 1)
     {
